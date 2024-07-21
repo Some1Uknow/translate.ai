d9bf862ceca268b8ffabbd543df55e59c5bb5a6d
Some1Uknow <raghu250407@gmail.com>
2024-07-21 10:02:52 +0530

    added keywords for better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,6 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Translate.ai",
   description: "Tranlate to your preferred language in just 1 click",
+  keywords: [
+    "Translate",
+    "Translator",
+    "Translate english to hindi",
+    "Translate hindi to english",
+    "Translate AI",
+    "AI Translator",
+    "Translate to any language",
+  ],
 };
 
 export default function RootLayout({
@@ -19,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} main-bg text-white`}>
         {children}
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
